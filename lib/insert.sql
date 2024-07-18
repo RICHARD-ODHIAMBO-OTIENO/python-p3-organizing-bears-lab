e8d389447740ae25b40010f32f52bba053eed88c
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-07-18 10:27:35 +0300

    Python_Organizing_Bears_Lab Initial Commit

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) 
+VALUES 
+  (1, 'Mr. Chocolate', 7, 'M', 'Brown', 'Gentle', TRUE),
+  (2, 'Rowdy', 5, 'M', 'Black', 'Aggressive', TRUE),
+  (3, 'Tabitha', 4, 'F', 'Cinnamon', 'Playful', TRUE),
+  (4, 'Sergeant Brown', 6, 'M', 'Grizzly', 'Stern', TRUE),
+  (5, 'Melissa', 3, 'F', 'Black', 'Curious', TRUE),
+  (6, 'Grinch', 8, 'M', 'Dark Brown', 'Grumpy', TRUE),
+  (7, 'Wendy', 2, 'F', 'Blonde', 'Friendly', TRUE),
+  (8, NULL, 1, 'F', 'White', 'Shy', TRUE);
